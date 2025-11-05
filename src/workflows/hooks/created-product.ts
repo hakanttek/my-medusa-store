72698c272af294a8b4aff1e0bce5260164313546
hakanttek <hakanttek@gmail.com>
2025-11-05 08:37:14 +0100

    feat(brand): link created products to brand in createProductsWorkflow - Added hook to associate products with a brand when created. - Utilizes BrandModuleService to retrieve brand and create link definitions. - Logs successful linking operation for better traceability.

---
+++ src/workflows/hooks/created-product.ts
@@ -0,0 +1,43 @@
+import { StepResponse } from "@medusajs/framework/workflows-sdk";
+import { createProductsWorkflow } from "@medusajs/medusa/core-flows";
+import { BRAND_MODULE } from "../../modules/brand";
+import BrandModuleService from "../../modules/brand/service";
+import { LinkDefinition } from "@medusajs/framework/types";
+import { Modules } from "@medusajs/framework/utils";
+import { Logger } from "@medusajs/medusa";
+import { Link } from "@medusajs/framework/modules-sdk";
+
+createProductsWorkflow.hooks.productsCreated(
+  (async ({ products, additional_data }, { container }) => {
+    if (!additional_data?.brand_id) {
+      return new StepResponse([], []);
+    }
+
+    const brandModuleService: BrandModuleService = container.resolve(BRAND_MODULE)
+
+    await brandModuleService.retrieveBrand(additional_data.brand_id as string)
+
+    // link brand to product
+    const link: Link = container.resolve("link")
+    const logger: Logger = container.resolve("logger")
+
+    const links: LinkDefinition[] = []
+
+    for (const product of products) {
+      links.push({
+        [Modules.PRODUCT]: {
+          product_id: product.id,
+        },
+        [BRAND_MODULE]: {
+          brand_id: additional_data.brand_id,
+        },
+      })
+    }
+
+    await link.create(links)
+
+    logger.info("Linked brand to products")
+
+    return new StepResponse(links, links)
+  })
+)
