271f47e8bdbe75a0f1661ec0d2625acdbc158385
hakanttek <hakanttek@gmail.com>
2025-11-10 00:40:22 +0100

    feat(api): add GET /admin/brands middleware with query validation
    - Added `validateAndTransformQuery` for handling GET requests to `/admin/brands`
    - Introduced `GetBrandsSchema` using `createFindParams` for query validation
    - Updated imports to include `validateAndTransformQuery` and `createFindParams`

--- src/api/middlewares.ts
+++ src/api/middlewares.ts
@@ -1,6 +1,9 @@
-import { defineMiddlewares, validateAndTransformBody, } from "@medusajs/framework/http"
+import { defineMiddlewares, validateAndTransformBody, validateAndTransformQuery, } from "@medusajs/framework/http"
 import { PostAdminCreateBrand } from "./admin/brands/validators"
 import { z } from 'zod'
+import { createFindParams } from "@medusajs/medusa/api/utils/validators"
+
+export const GetBrandsSchema = createFindParams();
 
 export default defineMiddlewares({
   routes: [
@@ -17,6 +20,23 @@ export default defineMiddlewares({
       additionalDataValidator: {
         brand_id: z.string().optional()
       }
+    },
+    {
+      matcher: "/admin/brands",
+      method: "GET",
+      middlewares: [
+        validateAndTransformQuery(
+          GetBrandsSchema,
+          {
+            defaults: [
+              "id",
+              "name",
+              "products.*",
+            ],
+            isList: true,
+          }
+        ),
+      ],
     }
   ],
 })
