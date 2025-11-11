17916852b2a50eff653f8127562ec8f727f798d3
hakanttek <hakanttek@gmail.com>
2025-11-12 00:43:51 +0100

    feat(admin): add brand list table with pagination and API integration

--- src/admin/routes/brands/page.tsx
+++ src/admin/routes/brands/page.tsx
@@ -1,13 +1,80 @@
 import { defineRouteConfig } from "@medusajs/admin-sdk";
-import { Container } from "@medusajs/ui"
-import { TagSolid } from "@medusajs/icons"
+import {
+  Container,
+  Heading,
+  createDataTableColumnHelper,
+  DataTable,
+  DataTablePaginationState,
+  useDataTable
+} from "@medusajs/ui";
+import { TagSolid } from "@medusajs/icons";
+import { useMemo, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { sdk } from "../../lib/sdk";
+
+type Brand = {
+  id: string
+  name: string
+}
+type BrandsResponse = {
+  brands: Brand[]
+  count: number
+  limit: number
+  offset: number
+}
+
+const columnHelper = createDataTableColumnHelper<Brand>();
+
+const columns = [
+  columnHelper.accessor("id", {
+    header: "ID"
+  }),
+  columnHelper.accessor("name", {
+    header: "Name"
+  })
+]
 
 const BrandPage = () => {
-  // TODO retrieve brands
+  const limit = 15;
+  const [pagination, setPagination] = useState<DataTablePaginationState>({
+    pageSize: limit,
+    pageIndex: 0
+  });
+  const offset = useMemo(() => {
+    return pagination.pageIndex * limit;
+  }, [pagination]);
+
+  const { data, isLoading } = useQuery<BrandsResponse>({
+    queryFn: () => sdk.client.fetch(`/admin/brands`, {
+      query: {
+        limit,
+        offset,
+      },
+    }),
+    queryKey: [["brands", limit, offset]],
+  });
+
+  const table = useDataTable({
+    columns,
+    data: data?.brands || [],
+    getRowId: (row) => row.id,
+    rowCount: data?.count || 0,
+    isLoading,
+    pagination: {
+      state: pagination,
+      onPaginationChange: setPagination
+    }
+  });
 
   return (
     <Container className="divide-y p-0">
-      {/* TODO show brands */}
+      <DataTable instance={table}>
+        <DataTable.Toolbar className="flex flex-col items-start justify-between gap-2 md:flex-row md:items-center">
+          <Heading>Brands</Heading>
+        </DataTable.Toolbar>
+        <DataTable.Table />
+        <DataTable.Pagination />
+      </DataTable>
     </Container>
   )
 }
