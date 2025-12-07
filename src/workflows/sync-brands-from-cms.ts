9aae234abb6b7780e28317fc7c25aa529fd0f86f
hakanttek <hakanttek@gmail.com>
2025-12-07 01:40:33 +0100

    feat(workflows): implement create brands step with CMS integration

--- src/workflows/sync-brands-from-cms.ts
+++ src/workflows/sync-brands-from-cms.ts
@@ -1,5 +1,7 @@
 import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk";
 import { CMS_MODULE } from "../modules/cms"
+import BrandModuleService from "../modules/brand/service"
+import brand, { BRAND_MODULE } from "../modules/brand"
 
 const retrieveBrandsFromCmsStep = createStep(
   "retrieve-brands-from-cms",
@@ -7,5 +9,31 @@ const retrieveBrandsFromCmsStep = createStep(
     const cmsModuleService = container.resolve(CMS_MODULE);
     const brands = await cmsModuleService.retrieveBrands();
     return new StepResponse(brands);
+  }
+)
+
+type CreateBrand = {
+  name: string;
+}
+
+type CreateBrandsInput = {
+  brands: CreateBrand[];
+}
+
+export const createBrandStep = createStep(
+  "create-brands-step",
+  async (input: CreateBrandsInput, { container }) => {
+    const brandModuleService = container.resolve<BrandModuleService>(BRAND_MODULE);
+    const brands = await brandModuleService.createBrands(input.brands);
+    return new StepResponse(brands, brands);
+  },
+  async (brands, { container }) => {
+    if (!brands) {
+      return;
+    }
+
+    const brandModuleService = container.resolve<BrandModuleService>(BRAND_MODULE);
+
+    await brandModuleService.deleteBrands(brands.map(b => b.id));
   }
 )
