6555542ce06460fd7278c4a1106b6103f1b313d2
hakanttek <hakanttek@gmail.com>
2025-12-07 10:49:32 +0100

    feat: add workflow steps for synchronizing brands from CMS

--- src/workflows/sync-brands-from-cms.ts
+++ src/workflows/sync-brands-from-cms.ts
@@ -1,7 +1,7 @@
 import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk";
 import { CMS_MODULE } from "../modules/cms"
 import BrandModuleService from "../modules/brand/service"
-import brand, { BRAND_MODULE } from "../modules/brand"
+import { BRAND_MODULE } from "../modules/brand"
 
 const retrieveBrandsFromCmsStep = createStep(
   "retrieve-brands-from-cms",
@@ -34,6 +34,40 @@ export const createBrandStep = createStep(
 
     const brandModuleService = container.resolve<BrandModuleService>(BRAND_MODULE);
 
-    await brandModuleService.deleteBrands(brands.map(b => b.id));
+    await brandModuleService.deleteBrands(brands.map(brand => brand.id));
+  }
+)
+
+type UpdateBrand = {
+  id: string;
+  name: string;
+}
+
+type UpdateBrandsInput = {
+  brands: UpdateBrand[];
+}
+
+export const updateBrandStep = createStep(
+  "update-brands-step",
+  async ({ brands }: UpdateBrandsInput, { container }) => {
+
+    const brandModuleService: BrandModuleService = container.resolve(BRAND_MODULE);
+
+    const prevUpdatedBrands = await brandModuleService.listBrands({
+      id: brands.map((brand) => brand.id),
+    })
+
+    const updatedBrands = await brandModuleService.updateBrands(brands);
+
+    return new StepResponse(updatedBrands, prevUpdatedBrands);
+  },
+  async (prevUpdatedBrands, { container }) => {
+    if (!prevUpdatedBrands) {
+      return;
+    }
+
+    const brandModuleService: BrandModuleService = container.resolve(BRAND_MODULE);
+
+    await brandModuleService.updateBrands(prevUpdatedBrands);
   }
 )
