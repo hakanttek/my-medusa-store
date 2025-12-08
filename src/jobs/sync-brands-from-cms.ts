fb29969bc1034eaf886dbd4765f4790e4ddc5f47
hakanttek <hakanttek@gmail.com>
2025-12-08 01:48:46 +0100

    feat: add job to synchronize brands from CMS

---
+++ src/jobs/sync-brands-from-cms.ts
@@ -0,0 +1,12 @@
+import { MedusaContainer } from "@medusajs/framework/types";
+import { syncBrandsFromCmsWorkflow } from "../workflows/sync-brands-from-cms";
+
+export default async function (container: MedusaContainer) {
+  const logger = container.resolve("logger")
+
+  const { result } = await syncBrandsFromCmsWorkflow(container).run()
+
+  logger.info(
+    `Synced brands from third-party system: ${result.created.length
+    } brands created and ${result.updated.length} brands updated.`)
+}
