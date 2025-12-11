63cb0f618cb144efcef317b7dbbeb2897b6ed7dd
hakanttek <hakanttek@gmail.com>
2025-12-11 04:27:55 +0100

    feat: implement createPostStep workflow with post creation and deletion logic

--- src/workflows/create-post.ts
+++ src/workflows/create-post.ts
@@ -1,5 +1,29 @@
-import { createStep } from "@medusajs/framework/workflows-sdk";
+import { createStep, StepResponse } from "@medusajs/framework/workflows-sdk"
+import { BlogModuleService } from "../modules/blog/service"
+import { BLOG_MODULE } from "../modules/blog"
+import { container } from "@medusajs/framework"
 
 type Input = {
-  title: string;
-};
+  title: string
+}
+
+const createPostStep = createStep(
+  "create-post",
+  async (input: Input, { container }) => {
+    const blogModuleService: BlogModuleService = container.resolve(
+      BLOG_MODULE
+    )
+
+    const post = await blogModuleService.createPosts(input)
+
+    return new StepResponse(post, post.id)
+  },
+  async (postId, { container }) => {
+    if (!postId)
+      return;
+
+    const blogModuleService: BlogModuleService = container.resolve(BLOG_MODULE)
+
+    await blogModuleService.deletePosts(postId)
+  }
+)
