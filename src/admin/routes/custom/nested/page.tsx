12bebca6ea2cd2a01bce9f5a9108facf922eec23
hakanttek <hakanttek@gmail.com>
2025-12-31 02:18:50 +0100

    feat(NestedCustomPage): create nested custom page component with breadcrumb

---
+++ src/admin/routes/custom/nested/page.tsx
@@ -0,0 +1,17 @@
+import { Container, Heading } from "@medusajs/ui"
+
+const NestedCustomPage = () => {
+  return (
+    <Container className="divide-y p-0">
+      <div className="flex items-center justify-between px-6 py-4">
+        <Heading level="h2">This is my nested custom route</Heading>
+      </div>
+    </Container>
+  )
+}
+
+export default NestedCustomPage
+
+export const handle = {
+  breadcrumb: () => "Nested Custom Route",
+}
