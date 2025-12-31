c92988562da4a5e83d9372d18e61a411b71db8b8
hakanttek <hakanttek@gmail.com>
2025-12-31 22:06:46 +0100

    feat(ProductReviewModuleService): add getAverageRating method to calculate average review rating

--- src/modules/product-review/service.ts
+++ src/modules/product-review/service.ts
@@ -1,9 +1,24 @@
-import { MedusaService } from "@medusajs/framework/utils"
+import { InjectManager, MedusaContext, MedusaService } from "@medusajs/framework/utils"
 import Review from "./models/review"
+import { EntityManager } from "@medusajs/framework/mikro-orm/knex"
+import { Context } from "@medusajs/framework/types"
 
 class ProductReviewModuleService extends MedusaService({
   Review,
 }) {
+  @InjectManager()
+  async getAverageRating(
+    productId: string,
+    @MedusaContext() sharedContext?: Context<EntityManager>
+  ): Promise<number> {
+    const result = await sharedContext?.manager?.execute(
+      `SELECT AVG(rating) as average 
+       FROM review 
+       WHERE product_id = '${productId}' AND status = 'approved'`
+    )
+
+    return parseFloat(parseFloat(result?.[0]?.average ?? 0).toFixed(2))
+  }
 }
 
 export default ProductReviewModuleService
