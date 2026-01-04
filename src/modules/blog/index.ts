f4eeed3d18d9a9d1a8e9c8e301db1d55fa955619
hakanttek <hakanttek@gmail.com>
2026-01-04 04:33:09 +0100

    refactor(blog\index): add loaders

--- src/modules/blog/index.ts
+++ src/modules/blog/index.ts
@@ -1,8 +1,11 @@
 import { Module } from "@medusajs/framework/utils";
 import { BlogModuleService } from "./service";
+import helloWorldLoader from "./loaders/hello-world";
+import validationLoader from "./loaders/validate";
 
 export const BLOG_MODULE = "blog";
 
 export default Module(BLOG_MODULE, {
-  service: BlogModuleService
+  service: BlogModuleService,
+  loaders: [helloWorldLoader, validationLoader],
 })
