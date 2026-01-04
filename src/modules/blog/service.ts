282cd2a1e5a69e304b6d23b67ece36724676d14b
hakanttek <hakanttek@gmail.com>
2026-01-04 04:33:40 +0100

    refactor(BlogModuleService): inejct options

--- src/modules/blog/service.ts
+++ src/modules/blog/service.ts
@@ -1,6 +1,18 @@
 import { MedusaService } from "@medusajs/framework/utils";
 import { Post } from "./models/post";
 
+type ModuleOptions = {
+  capitalize?: boolean
+}
+
 export class BlogModuleService extends MedusaService({ Post }) {
 
+  protected options_: ModuleOptions;
+
+  constructor({ }, options?: ModuleOptions) {
+    super(...arguments);
+    this.options_ = options || {
+      capitalize: false
+    };
+  }
 }
