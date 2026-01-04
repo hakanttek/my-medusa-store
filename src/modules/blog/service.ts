254a931f00cd83c706f9b62eb66b70fb0faab907
hakanttek <hakanttek@gmail.com>
2026-01-04 19:20:04 +0100

    feat (BlogModuleService): add update and getCount methods

--- src/modules/blog/service.ts
+++ src/modules/blog/service.ts
@@ -1,5 +1,11 @@
-import { MedusaService } from "@medusajs/framework/utils";
+import { InjectTransactionManager, MedusaService } from "@medusajs/framework/utils";
 import { Post } from "./models/post";
+import {
+  InjectManager,
+  MedusaContext,
+} from "@medusajs/framework/utils"
+import { Context } from "@medusajs/framework/types"
+import { EntityManager } from "@medusajs/framework/mikro-orm/knex"
 
 type ModuleOptions = {
   capitalize?: boolean
@@ -14,5 +20,36 @@ export class BlogModuleService extends MedusaService({ Post }) {
     this.options_ = options || {
       capitalize: false
     };
+  }
+
+  @InjectTransactionManager()
+  protected async update_(
+    input: {
+      id: string,
+      name: string
+    },
+    @MedusaContext() sharedContext?: Context<EntityManager>
+  ): Promise<any> {
+    const transactionManager = sharedContext?.transactionManager
+
+    // TODO: update the record
+  }
+
+  @InjectManager()
+  async update(
+    input: {
+      id: string,
+      name: string
+    },
+    @MedusaContext() sharedContext?: Context<EntityManager>
+  ) {
+    return await this.update_(input, sharedContext)
+  }
+
+  @InjectManager()
+  async getCount(
+    tableName: string,
+    @MedusaContext() sharedContext?: Context<EntityManager>): Promise<number | undefined> {
+    return await sharedContext?.manager?.count(tableName)
   }
 }
