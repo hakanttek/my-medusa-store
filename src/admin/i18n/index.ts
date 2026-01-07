4e414f5f9c7092b684854085a15954a2d86c1c0e
hakanttek <hakanttek@gmail.com>
2026-01-07 04:11:55 +0100

    add json files for i18n

--- src/admin/i18n/index.ts
+++ src/admin/i18n/index.ts
@@ -1,1 +1,11 @@
-export default {}
+import en from "./json/en.json" with { type: "json" }
+import es from "./json/es.json" with { type: "json" }
+
+export default {
+  en: {
+    translation: en,
+  },
+  es: {
+    translation: es,
+  },
+}
